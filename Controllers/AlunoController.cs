8556122b84271fdbe9fa15bd31d198403e4fd71e
Glysonn <kauaglyson@gmail.com>
2023-01-02 19:13:12 -0300

    Implementando a listagem dos alunos

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using dotnetmvc.Context;
 namespace dotnetmvc.Controllers
 {
     public class AlunoController : Controller
     {
+        // criando uma propriedade do tipo EscolaContext (o banco de dados)
+        private readonly EscolaContext _context;
+
+        // atribuindo à propriedade _context o valor que é o próprio banco de dados
+        public AlunoController (EscolaContext context)
+        {
+            _context = context;
+        }
+
+        // rota index
         public IActionResult Index()
         {
-            return View();
+            var alunos = _context.Alunos.ToList();
+            return View(alunos);
         }
     }
 }
